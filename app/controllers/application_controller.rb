25fe210b86231b41929ae9972b6d1e6e0273d243
Spencer Alan <spencer.alan@icloud.com>
2017-07-21 16:51:57 -0700

    add rescue methods for errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
   include Response
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({ message: exception.message }, :unprocessable_entity)
+  end
+
 private
   def authenticate
     head :unauthorized unless User.exists?(token: params[:token])
