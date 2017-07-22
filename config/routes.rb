44b312415328c73c9c7f177674656d1176fe9d7a
Spencer Alan <spencer.alan@icloud.com>
2017-07-21 17:30:52 -0700

    reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :businesses
 
-  get "businesses/random", to: "businesses#random"
+  get "businesses/random/", to: "businesses#random"
   
-  get "businesses/search", to: "businesses#search"
+  get "businesses/search/", to: "businesses#search"
+
+  resources :businesses
 
   match "*path", to: -> (env) { [404, {}, ['{"error": "not_found"}']] }, via: :all
 end
