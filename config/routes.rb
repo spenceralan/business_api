8ce4f317f81b560da414c1262f4b501ab1e9ad33
Spencer Alan <spencer.alan@icloud.com>
2017-07-21 17:11:00 -0700

    custom random and search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :businesses
 
+  get "businesses/random", to: "businesses#random"
+  
+  get "businesses/search", to: "businesses#search"
+
   match "*path", to: -> (env) { [404, {}, ['{"error": "not_found"}']] }, via: :all
 end
