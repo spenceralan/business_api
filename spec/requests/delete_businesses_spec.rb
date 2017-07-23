e5c307397647c71c5bbfbe36d2c12830f9d5ee4b
Spencer Alan <spencer.alan@icloud.com>
2017-07-23 10:38:57 -0700

    update specs

--- spec/requests/delete_businesses_spec.rb
+++ spec/requests/delete_businesses_spec.rb
@@ -1,16 +1,26 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "authentication for delete requests", type: :request do
-#   let!(:user) {FactoryGirl.create(:user)}
+describe "authentication for delete requests", type: :request do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:business) { FactoryGirl.create(:business) }
+  let!(:businesses) { FactoryGirl.create_list(:business, 20) }
 
-#   it 'returns status code 200 for an authenticated user and a delete request' do
-#     delete '/businesses', params: {token: user.token}
-#     expect(response).to have_http_status(:success)
-#   end
+  it 'returns status code 200 for an authenticated user and a delete request' do
+    delete "/businesses/#{business.id}", params: { token: user.token }
+    expect(response).to have_http_status(:success)
+  end
 
-#   it 'returns status code 401 for an authenticated user and a delete request' do
-#     delete '/businesses'
-#     expect(response).to have_http_status(:unauthorized)
-#   end
+  it 'returns status code 401 for an unauthenticated user and a delete request' do
+    delete "/businesses/#{business.id}"
+    expect(response).to have_http_status(:unauthorized)
+  end
 
-# end
+  it 'should have one less entry after sucessful delete request' do
+    get "/businesses/", params: { token: user.token }
+    expect(JSON.parse(response.body).size).to eq(21)
+    delete "/businesses/#{business.id}", params: { token: user.token }
+    get "/businesses/", params: { token: user.token }
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+
+end
--- spec/requests/put_businesses_spec.rb
+++ spec/requests/put_businesses_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 describe "authentication for put requests", type: :request do
-  let!(:user) {FactoryGirl.create(:user)}
-  let!(:business) {FactoryGirl.create(:business)}
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:business) { FactoryGirl.create(:business) }
 
 
   it 'returns status code 200 for an authenticated user and a put request' do
-    put "/businesses/#{business.id}", params: {token: user.token}
+    put "/businesses/#{business.id}", params: { token: user.token }
     expect(response).to have_http_status(:success)
   end
 
   it 'returns status code 401 for an unauthenticated user and a put request' do
     put "/businesses/#{business.id}"
     expect(response).to have_http_status(:unauthorized)
+  end
+
+  it 'updates the attributes of a businesss' do
+    put "/businesses/#{business.id}", params: { token: user.token, "name" => "Test Business" }
+    expect(response).to have_http_status(:success)
+    get "/businesses/#{business.id}", params: { token: user.token }
+    expect(JSON.parse(response.body)["name"]).to eq("Test Business")
   end
 
 end
