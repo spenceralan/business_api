cdb759cc97266110373100f22c7a5b764c789720
Spencer Alan <spencer.alan@icloud.com>
2017-07-22 22:20:06 -0700

    refactor with scopes

--- app/models/business_query.rb
+++ app/models/business_query.rb
@@ -32,51 +32,51 @@ class BusinessQuery
     conditions = []
 
     if params.key? :name
-      conditions << Business.where("name ilike ?", "%#{params.fetch(:name)}%")
+      conditions << Business.by_name(params.fetch(:name))
     end
 
     if params.key? :phone
-      conditions << Business.where("phone ilike ?", "%#{params.fetch(:phone)}%")
+      conditions << Business.by_phone(params.fetch(:phone))
     end
 
     if params.key? :email
-      conditions << Business.where("email ilike ?", "%#{params.fetch(:email)}%")
+      conditions << Business.by_email(params.fetch(:email))
     end
 
     if params.key? :twitter
-      conditions << Business.where("twitter ilike ?", "%#{params.fetch(:twitter)}%")
+      conditions << Business.by_twitter(params.fetch(:twitter))
     end
 
     if params.key? :facebook
-      conditions << Business.where("facebook ilike ?", "%#{params.fetch(:facebook)}%")
+      conditions << Business.by_facebook(params.fetch(:facebook))
     end
 
     if params.key? :website
-      conditions << Business.where("website ilike ?", "%#{params.fetch(:website)}%")
+      conditions << Business.by_website(params.fetch(:website))
     end
 
     if params.key? :owner
-      conditions << Business.where("owner ilike ?", "%#{params.fetch(:owner)}%")
+      conditions << Business.by_owner(params.fetch(:owner))
     end
 
     if params.key? :address1
-      conditions << Business.where("address1 ilike ?", "%#{params.fetch(:address1)}%")
+      conditions << Business.by_address1(params.fetch(:address1))
     end
 
     if params.key? :address2
-      conditions << Business.where("address2 ilike ?", "%#{params.fetch(:address2)}%")
+      conditions << Business.by_address2(params.fetch(:address2))
     end
 
     if params.key? :city
-      conditions << Business.where("city ilike ?", "%#{params.fetch(:name)}%")
+      conditions << Business.by_city(params.fetch(:city))
     end
 
     if params.key? :state
-      conditions << Business.where("state ilike ?", "%#{params.fetch(:state)}%")
+      conditions << Business.by_state(params.fetch(:state))
     end
 
     if params.key? :zip
-      conditions << Business.where("zip ilike ?", "%#{params.fetch(:zip)}%")
+      conditions << Business.by_zip(params.fetch(:zip))
     end
 
     conditions.inject(Business.default_scoped, :merge)
