ff9c1f3a24bf8a32bbdcd11800660d6abe02c459
Spencer Alan <spencer.alan@icloud.com>
2017-07-22 22:19:46 -0700

    add scopes to the model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,18 @@
 class Business < ApplicationRecord
   
+
+  scope :by_name, -> (name) { where("name ilike ?", "%#{name}%") }
+  scope :by_phone, -> (phone) { where("phone ilike ?", "%#{phone}%") }
+  scope :by_email, -> (email) { where("email ilike ?", "%#{email}%") }
+  scope :by_twitter, -> (twitter) { where("twitter ilike ?", "%#{twitter}%") }
+  scope :by_facebook, -> (facebook) { where("facebook ilike ?", "%#{facebook}%") }
+  scope :by_website, -> (website) { where("website ilike ?", "%#{website}%") }
+  scope :by_owner, -> (owner) { where("owner ilike ?", "%#{owner}%") }
+  scope :by_address1, -> (address1) { where("address1 ilike ?", "%#{address1}%") }
+  scope :by_address2, -> (address2) { where("address2 ilike ?", "%#{address2}%") }
+  scope :by_city, -> (city) { where("city ilike ?", "%#{city}%") }
+  scope :by_state, -> (state) { where("state ilike ?", "%#{state}%") }
+  scope :by_zip, -> (zip) { where("zip ilike ?", "%#{zip}%") }
+
+
 end
