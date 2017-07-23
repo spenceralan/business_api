cd528e5b9e90fd14f3ebdbc5f5b3d0a6528fe65e
Spencer Alan <spencer.alan@icloud.com>
2017-07-23 10:09:54 -0700

    update specs

--- spec/requests/post_businesses_spec.rb
+++ spec/requests/post_businesses_spec.rb
@@ -9,9 +9,28 @@ describe "authentication for post requests", type: :request do
     expect(response).to have_http_status(:unprocessable_entity)
   end
 
-  it 'returns status code 401 for an authenticated user and a post request' do
+  it 'returns status code 401 for an unauthenticated user and a post request' do
     post "/businesses/"
     expect(response).to have_http_status(:unauthorized)
+  end
+
+  it 'returns status code 200 when an entry is sucessfully created' do
+    post "/businesses/", params: {
+      token: user.token,
+      name: Faker::Company.name,
+      phone: Faker::PhoneNumber.phone_number,
+      email: Faker::Internet.email,
+      twitter: Faker::Internet.url('twitter.com'),
+      facebook: Faker::Internet.url('facebook.com'),
+      website: Faker::Internet.url,
+      owner: Faker::Name.name,
+      address1: Faker::Address.street_address,
+      address2: Faker::Address.secondary_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip_code,
+    }
+    expect(response).to have_http_status(:success)
   end
 
 end
--- spec/requests/put_businesses_spec.rb
+++ spec/requests/put_businesses_spec.rb
@@ -1,16 +1,18 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "authentication for put requests", type: :request do
-#   let!(:user) {FactoryGirl.create(:user)}
+describe "authentication for put requests", type: :request do
+  let!(:user) {FactoryGirl.create(:user)}
+  let!(:business) {FactoryGirl.create(:business)}
 
-#   it 'returns status code 200 for an authenticated user and a put request' do
-#     put '/businesses', params: {token: user.token}
-#     expect(response).to have_http_status(:success)
-#   end
 
-#   it 'returns status code 401 for an authenticated user and a put request' do
-#     put '/businesses'
-#     expect(response).to have_http_status(:unauthorized)
-#   end
+  it 'returns status code 200 for an authenticated user and a put request' do
+    put "/businesses/#{business.id}", params: {token: user.token}
+    expect(response).to have_http_status(:success)
+  end
 
-# end
+  it 'returns status code 401 for an unauthenticated user and a put request' do
+    put "/businesses/#{business.id}"
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+end
