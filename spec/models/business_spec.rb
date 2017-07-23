a36e52e08090c93d065b9ab02ecadc9c908e80db
Spencer Alan <spencer.alan@icloud.com>
2017-07-22 23:15:11 -0700

    add validation tests

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-
+describe Business do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :phone }
+  it { should validate_presence_of :address1 }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :state }
+  it { should validate_presence_of :zip }
 end
